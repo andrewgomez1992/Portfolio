8b303031f03401e60f9cb6a8663a1c4c4549d7a3
Andrew Gomez <drewgomez209@gmail.com>
2022-06-20 13:08:44 -0700

    addedlink for email on topbar

--- myportfolio/src/components/topbar/Topbar.jsx
+++ myportfolio/src/components/topbar/Topbar.jsx
@@ -13,7 +13,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
                     </div>
                     <div className="itemContainer">
                         <Mail className="icon" />
-                        <span>drewgomez209@gmail.com</span>
+                        <span><a href="mailto:drewgomez209@gmail.com">drewgomez209@gmail.com</a></span>
                     </div>
                 </div>
 
