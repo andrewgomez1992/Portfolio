256fca3e2db3c10af8ab6aab3c341978609da9b2
Andrew Gomez <drewgomez209@gmail.com>
2022-07-26 13:49:45 -0700

    responsive test3

--- myportfolio/src/components/contact/Contact.jsx
+++ myportfolio/src/components/contact/Contact.jsx
@@ -3,6 +3,10 @@ import React from "react";
 
 export default function Intro() {
 
+    const linkedInURL = 'https://www.linkedin.com/in/andrewgomez1992'
+    const instagramURL = 'https://www.instagram.com/_drewgomez_/'
+    const githubURL = 'https://github.com/andrewgomez1992'
+
     return (
         <div className="aboutintro" id="contact">
             <div className="aboutleft">
@@ -12,7 +16,14 @@ export default function Intro() {
             </div>
             <div className='aboutme'>
                 {/* <h1>A little more about myself...</h1> */}
-                <p>I am a Full Stack Software Engineer who will be graduating from Bloom Institute of Technology in August 2022. I have spent hundreds of hours with JavaScript, React, HTML, CSS, API's, SQL and more which have thrusted me into a bright introduction of a world I once never knew existed.</p>
+                <p>I am a Full Stack Software Engineer who will be graduating from Bloom Institute of Technology in August 2022.
+                    I have spent hundreds of hours with JavaScript, React, HTML, CSS, API's, SQL and more which have thrusted me
+                    into a bright introduction of a world I once never knew existed.</p>
+            </div>
+            <div className='socialmedia'>
+                <a onClick={() => window.open(linkedInURL, '_blank')} href="#" class="fa fa-linkedin"></a>
+                <a onClick={() => window.open(githubURL, '_blank')} href="#" class="fa fa-github"></a>
+                <a onClick={() => window.open(instagramURL, '_blank')} href="#" class="fa fa-instagram"></a>
             </div>
         </div >
     )
